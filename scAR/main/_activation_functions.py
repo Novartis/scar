acc8d4fc0b65592b4810e9ddadb3c3f505f8789a
Gypas, Foivos <foivos.gypas@novartis.com>
2022-03-23 08:19:12 -0400

    Format code with black. Use --line-length: 127 in the github action.

--- scAR/main/_activation_functions.py
+++ scAR/main/_activation_functions.py
@@ -2,17 +2,20 @@
 
 import torch
 
+
 def mytanh(x):
     a = x.exp()
-    tanh = (a - 1/a) / (a + 1/a)
+    tanh = (a - 1 / a) / (a + 1 / a)
     tanh = (1 + tanh) / 2
     return tanh
 
+
 def hnormalization(x):
-    return x/(x.sum(dim=1).view(-1,1) + 1e-5)
+    return x / (x.sum(dim=1).view(-1, 1) + 1e-5)
+
 
 def mySoftplus(x):
-    mask0 = x<=-5
+    mask0 = x <= -5
     x[mask0] = 0
-    x[~mask0] = (x[~mask0].exp()+1).log()
+    x[~mask0] = (x[~mask0].exp() + 1).log()
     return x
