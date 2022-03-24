221caa04221bf8e6f75c643c4ec39289cdcd6bdc
Gypas, Foivos <foivos.gypas@novartis.com>
2022-03-24 09:18:02 -0400

    Pylint for __init__.py and _activation_functions.py

--- scAR/main/_activation_functions.py
+++ scAR/main/_activation_functions.py
@@ -1,9 +1,9 @@
+"""Activation functions"""
 # -*- coding: utf-8 -*-
-
-import torch
 
 
 def mytanh(x):
+    """mytanh"""
     a = x.exp()
     tanh = (a - 1 / a) / (a + 1 / a)
     tanh = (1 + tanh) / 2
@@ -11,10 +11,12 @@ def mytanh(x):
 
 
 def hnormalization(x):
+    """hnormalization"""
     return x / (x.sum(dim=1).view(-1, 1) + 1e-5)
 
 
 def mySoftplus(x):
+    """mySoftplus"""
     mask0 = x <= -5
     x[mask0] = 0
     x[~mask0] = (x[~mask0].exp() + 1).log()
