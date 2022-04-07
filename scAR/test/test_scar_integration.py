fc17e6649421ad58b74f55303a5f2ff8ecc2b6df
Gypas, Foivos <foivos.gypas@novartis.com>
2022-04-07 12:04:04 +0200

    Add integration test for scAR

---
+++ scAR/test/test_scar_integration.py
@@ -0,0 +1,26 @@
+from scAR import model
+import pandas as pd
+import sys
+import os
+
+raw_count = pd.read_pickle('raw_counts.pickle')
+empty_profile = pd.read_pickle('ambient_profile.pickle')
+expected_output = pd.read_pickle('output_assignment.pickle')
+
+scarObj = model(raw_count=raw_count.values,
+                empty_profile=empty_profile,
+                scRNAseq_tech='CROPseq')
+
+scarObj.train(epochs=40,
+              batch_size=64,)
+
+scarObj.inference()
+
+status = scarObj.feature_assignment.equals(expected_output)
+
+if status:
+    sys.stdout.write(f"Sucessful integration test.{os.linesep}")
+    sys.exit(0)
+else:
+    sys.stdout.write(f"Error in integration test.{os.linesep}")
+    sys.exit(1)
