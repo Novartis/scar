de267ed6546fd9e1aba50594223bbddc57199f56
Sheng, Caibin <caibin.sheng@novartis.com>
2022-05-20 11:33:59 +0200

    docs: change background of logo

--- docs/conf.py
+++ docs/conf.py
@@ -79,7 +79,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_logo = "img/scAR_logo2.png"
+html_logo = "img/scAR_logo_transparent.png"
 html_theme_options = {
     "logo_only": True,
     "display_version": True,
