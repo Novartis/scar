9e3fd55c062940e42e646fcf3b37dabf6f67b314
Caibin Sheng <43896555+CaibinSh@users.noreply.github.com>
2024-05-23 19:17:02 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -73,6 +73,7 @@ html_theme = "pydata_sphinx_theme"
 templates_path = ["_templates"]
 html_static_path = ["_static"]
 html_css_files = ["scar-styles.css"]
+html_show_sourcelink = False
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
