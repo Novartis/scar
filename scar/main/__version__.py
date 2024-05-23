23ed4a7a5c297894bc4309a77ec31b8a8a1587dd
Caibin Sheng <caibin.sheng.res@gmail.com>
2024-05-23 11:04:27 +0200

    fix: update the action file

--- scar/main/__version__.py
+++ scar/main/__version__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """version here"""
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
 _author = "Caibin Sheng"
 _copyright = "2022, Novartis Institute for BioMedical Research"
