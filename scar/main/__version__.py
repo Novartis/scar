0af992265ef2a2e4f0c6a497d425c42902a72f49
Caibin Sheng <43896555+CaibinSh@users.noreply.github.com>
2024-05-23 19:31:47 +0200

    Update __version__.py

--- scar/main/__version__.py
+++ scar/main/__version__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """version here"""
 
-__version__ = "0.5.3"
+__version__ = "0.5.4"
 _author = "Caibin Sheng"
 _copyright = "2022, Novartis Institute for BioMedical Research"
